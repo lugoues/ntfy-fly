db43ef5e72b5190a715f9d6864aae6e175d8e26a
Peter Brunner <peter@lugoues.net>
2026-08-08 21:51:56 -0400

    more auth

--- rootfs/app/startup.sh
+++ rootfs/app/startup.sh
@@ -13,16 +13,6 @@ ntfy_pid=$!
 # Wait for ntfy to start
 sleep 5
 
-# Check if NTFY_PASSWORD is empty
-if [ -z "$NTFY_PASSWORD" ]; then
-  # NTFY_PASSWORD is empty, set a new value
-  # Replace "your-password-here" with the actual password for initialization
-  export NTFY_PASSWORD="default_password"
-else
-  # NTFY_PASSWORD is not empty, don't overwrite it
-  echo "NTFY_PASSWORD is already set, not overwriting."
-fi
-
 # Check if the user database exists. If not, create an initial user.
 if [ ! -f "${NTFY_AUTH_FILE}" ]; then
   echo "Creating initial user..."
